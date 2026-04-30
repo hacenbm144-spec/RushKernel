a44bd46633483bbb3223af60cf1d1eb4d98ace3e
hacenbm144-spec <hacenbm144@gmail.com>
2026-04-30 18:44:42 +0100

    debug: maximum verbose flashing diagnostics

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -1,14 +1,14 @@
 ### AnyKernel3 - RushKernel for Samsung Galaxy S22 Ultra Exynos (b0s)
-### Maintained by hacenbm144 | Based on osm0sis AnyKernel3
+### DEBUG VERSION - shows every operation
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=RushKernel by hacenbm144
+kernel.string=RushKernel by hacenbm144 [DEBUG BUILD]
 do.devicecheck=1
 do.modules=0
 do.systemless=1
-do.cleanup=1
+do.cleanup=0
 do.cleanuponabort=0
 device.name1=b0s
 device.name2=SM-S908B
@@ -25,10 +25,42 @@ ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
 ## AnyKernel install
-set -x;
 . tools/ak3-core.sh;
 
-## boot install
+# Pre-install diagnostics
+ui_print " ";
+ui_print "=== RushKernel Debug Info ===";
+ui_print "Looking for boot block...";
+ui_print "Block: $block";
+
+# Try unpacking with verbose magiskboot output
+ui_print " ";
+ui_print "=== Manual unpack test ===";
+cd $home;
+dump_boot;
+ui_print "Boot image dumped to $home/boot.img";
+ls -la $home/boot.img 2>&1 | while read line; do ui_print "$line"; done;
+
+ui_print " ";
+ui_print "=== Magiskboot unpack ===";
+cd $split_img;
+$bin/magiskboot unpack -h boot.img 2>&1 | while read line; do ui_print "$line"; done;
+ls -la 2>&1 | while read line; do ui_print "$line"; done;
+
+ui_print " ";
+ui_print "=== Replacing kernel ===";
+ls -la $home/Image 2>&1 | while read line; do ui_print "$line"; done;
+cp -f $home/Image kernel;
+ls -la kernel 2>&1 | while read line; do ui_print "$line"; done;
+
+ui_print " ";
+ui_print "=== Repack attempt ===";
+$bin/magiskboot repack boot.img new-boot.img 2>&1 | while read line; do ui_print "$line"; done;
+ls -la new-boot.img 2>&1 | while read line; do ui_print "$line"; done;
+
+ui_print " ";
+ui_print "=== Writing boot ===";
 write_boot;
 
-## end install
+ui_print " ";
+ui_print "=== Done ===";
